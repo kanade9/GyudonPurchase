d0d032284d74bfdd300b1782cf852e29b3b472c0
kzmaro <maronkazu94@gmail.com>
2019-04-27 01:13:22 +0900

    add'MainShow'

---
+++ src/components/MainShow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>
+            This is MainShow
+        </p>
+        <h4>This page will be a top page</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainShow",
+        data: function () {
+            return {}
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
