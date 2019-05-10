9ac12cae4c1ce3807c0d719054c58fe9ef5856d7
kzmaro <maronkazu94@gmail.com>
2019-05-11 00:51:20 +0900

    pdf

--- src/components/AppMenuButton.vue
+++ src/components/AppMenuButton.vue
@@ -1,19 +1,28 @@
 <template>
-    <button type="button" v-on:click='tell()'>
-        <span style="font-size: large; color: #333399; ">{{menu}}</span>
-    </button>
+    <!--<v-container>-->
+    <v-layout justify-center>
+        <v-card>
+            <v-card-actions>
+                <v-btn flat color="orange" type="button" v-on:click='tell()'>
+                    <span style=" color: #333399; ">{{menu}}</span>
+                    <span style=" color: brown">{{A}}円</span>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-layout>
+    <!--</v-container>-->
 </template>
 
 <script>
     export default {
         name: "AppMenuButton",
-        props: ['menu','N','S','O','A'],
-        data: function(){
-            return{}
+        props: ['menu', 'N', 'S', 'O', 'A'],
+        data: function () {
+            return {}
         },
-        methods:{
-            tell: function(){
-                this.$emit('tellValue',this.menu,this.N,this.S,this.O,this.A)
+        methods: {
+            tell: function () {
+                this.$emit('tellValue', this.menu, this.N, this.S, this.O, this.A)
             }
         }
     }
